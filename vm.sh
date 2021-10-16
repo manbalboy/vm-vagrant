09a0997714e926641aecdec1980753ae5ef19cba
manbalboy <manbalboy@hanmail.net>
2021-10-17 00:21:02 +0900

    쿠버네티스 학습

--- vm.sh
+++ vm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VBoxManage startvm "k8s-vm_master_1634224058287_96105" --type headless
-VBoxManage startvm "k8s-vm_w-k8s-worker1_1634224253536_70692" --type headless
-VBoxManage startvm "k8s-vm_w-k8s-worker2_1634224468212_81817" --type headless
-
+VBoxManage startvm "m-k8s-1.20(github_SysNet4Admin)"  --type headless
+VBoxManage startvm "w1-k8s-1.20(github_SysNet4Admin)" --type headless
+VBoxManage startvm "w2-k8s-1.20(github_SysNet4Admin)" --type headless
+VBoxManage startvm "w3-k8s-1.20(github_SysNet4Admin)" --type headless
