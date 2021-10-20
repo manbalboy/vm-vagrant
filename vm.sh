c5daaac1f5ff74a72982bb03b1be3fd89f814053
manbalboy <manbalboy@hanmail.net>
2021-10-20 23:21:28 +0900

    vm.sh 변경

--- vm.sh
+++ vm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-VBoxManage startvm "m-k8s-1.20(github_SysNet4Admin)"  --type headless
-VBoxManage startvm "w1-k8s-1.20(github_SysNet4Admin)" --type headless
-VBoxManage startvm "w2-k8s-1.20(github_SysNet4Admin)" --type headless
-VBoxManage startvm "w3-k8s-1.20(github_SysNet4Admin)" --type headless
+VBoxManage startvm "WO-m-k8s-1.20(github_manbalboy)" --type headless
+VBoxManage startvm "WO-w1-k8s-1.20(github_manbalboy)" --type headless
+VBoxManage startvm "WO-w2-k8s-1.20(github_manbalboy)" --type headless
+VBoxManage startvm "WO-w3-k8s-1.20(github_manbalboy)" --type headless
+
