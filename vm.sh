e9da1bd0dcf854d16347734e17849b4b27382a6e
manbalboy <manbalboy@hanmail.net>
2021-10-21 00:28:41 +0900

    실습환경 변경

--- vm.sh
+++ vm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-VBoxManage startvm "WO-m-k8s-1.20(github_manbalboy)" --type headless
-VBoxManage startvm "WO-w1-k8s-1.20(github_manbalboy)" --type headless
-VBoxManage startvm "WO-w2-k8s-1.20(github_manbalboy)" --type headless
-VBoxManage startvm "WO-w3-k8s-1.20(github_manbalboy)" --type headless
+VBoxManage startvm "master-k8s" --type headless
+VBoxManage startvm "worker1-k8s" --type headless
+VBoxManage startvm "worker2-k8s" --type headless
+VBoxManage startvm "worker3-k8s" --type headless
 
